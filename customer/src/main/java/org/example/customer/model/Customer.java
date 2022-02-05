f3dfcbcfe7368167e742f13da95aa434c1534eb5
Emmanuel Huitrado <emmanuel.huitrado@unosquare.com>
2022-02-05 13:38:52 -0600

    Created the complete structure for customers microservice

--- customer/src/main/java/org/example/customer/model/Customer.java
+++ customer/src/main/java/org/example/customer/model/Customer.java
@@ -2,16 +2,54 @@ package org.example.customer.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+import javax.persistence.*;
+
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
-    private Integer id;
+
+    @Id
+    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
+    private Long id;
+
     private String firstName;
     private String lastName;
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- customer/src/main/java/org/example/customer/service/CustomerService.java
+++ customer/src/main/java/org/example/customer/service/CustomerService.java
@@ -1,19 +1,26 @@
 package org.example.customer.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.customer.model.Customer;
+import org.example.customer.repository.CustomerRepository;
 import org.example.customer.request.CustomerRegistrationRequest;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder().
                             firstName(request.getFirstName())
-                            .lastName(request.getFirstName())
+                            .lastName(request.getLastName())
                             .email(request.getEmail())
                             .build();
+        //todo: validate fields
+        customerRepository.save(customer);
         log.info("Customer {} saved on the db", customer);
     }
 }
