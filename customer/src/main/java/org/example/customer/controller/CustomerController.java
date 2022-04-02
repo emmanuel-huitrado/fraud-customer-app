b2b013c2932d4ea8ea381b6ead9e80de7ca71da1
Emmanuel Huitrado <emmanuel.huitrado@unosquare.com>
2022-04-01 19:37:42 -0600

    Adding Sleuth, Zipkin and API Gateway

--- customer/src/main/java/org/example/customer/controller/CustomerController.java
+++ customer/src/main/java/org/example/customer/controller/CustomerController.java
@@ -2,12 +2,12 @@ package org.example.customer.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.customer.model.Customer;
 import org.example.customer.request.CustomerRegistrationRequest;
 import org.example.customer.service.CustomerService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
